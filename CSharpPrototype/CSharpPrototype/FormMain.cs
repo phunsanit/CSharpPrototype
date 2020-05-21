71be7a194f24f6821efe8d95107c69c2fc326adc
pitt phunsanit <phunsanit@gmail.com>
2020-05-21 21:40:39 +0700

    no message

---
+++ CSharpPrototype/CSharpPrototype/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CSharpPrototype
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            AssemblyName thisAssemName = this.GetType().Assembly.GetName();
+
+            this.Text = thisAssemName.Name + " Version " + thisAssemName.Version;
+        }
+
+    }
+}
